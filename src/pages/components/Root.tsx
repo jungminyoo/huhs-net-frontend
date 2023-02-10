e8abd3b3c3d20b3e8194cca16a58f7170ae0fec6
jungminyoo <yoojm4718@outlook.com>
2023-02-11 01:24:33 +0900

    style : Styled RootContainer

--- src/pages/components/Root.tsx
+++ src/pages/components/Root.tsx
@@ -1,13 +1,18 @@
 import { Outlet } from "react-router-dom";
+import styled from "styled-components";
 import Header from "../../shared/components/Header";
 
 const Root = () => {
   return (
-    <div>
+    <RootContainer>
       <Header />
       <Outlet />
-    </div>
+    </RootContainer>
   );
 };
+
+const RootContainer = styled.div`
+  margin-top: ${(props) => props.theme.headerHeight}px;
+`;
 
 export default Root;
